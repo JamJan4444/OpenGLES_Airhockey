d04c0df78e4ff52ca177761231158c467f6c49cc
Jan Rensing <jan.rensing@gmail.com>
2016-12-03 14:28:05 +0100

    Add Basic Scene from Getting started

---
+++ app/src/main/java/com/airhockey/opengles_airhockey/FirstOpenGLProjectRenderer.java
@@ -0,0 +1,35 @@
+package com.airhockey.opengles_airhockey;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by jan on 03.12.16.
+ */
+
+public class FirstOpenGLProjectRenderer implements GLSurfaceView.Renderer {
+
+    @Override
+    public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+
+        //Specify Background Color
+        GLES20.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl10, int width, int height) {
+
+        // Set the OpenGL viewport to fill the entire surface.
+        GLES20.glViewport(0, 0, width, height);
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl10) {
+        // Clear the rendering surface.
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+    }
+}
